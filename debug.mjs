84e6a518c9296be5871b69f52e33aa6d0da879e1
Alysson Jose <alyssonjoseoficial@gmail.com>
2026-06-28 23:29:28 -0300

    Remove temporary test scripts

--- debug.mjs
+++
@@ -1,9 +0,0 @@
-import { PrismaClient } from '@prisma/client';
-const prisma = new PrismaClient();
-async function run() {
-  const plans = await prisma.plan.findMany();
-  console.log("Plans:", JSON.stringify(plans, null, 2));
-  const invoices = await prisma.invoice.findMany();
-  console.log("Invoices:", JSON.stringify(invoices, null, 2));
-}
-run();
